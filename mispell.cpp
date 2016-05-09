91ca80add298d5fe87c021399713a10f7c237371
“Joowon” <joowon7508@gmail.com>
2016-05-09 20:38:07 +0900

    used just one array

--- mispell.cpp
+++ mispell.cpp
@@ -12,24 +12,23 @@ using namespace std;
 
 int main() {
   int num, wordlen;
-  char word[80], outword[80];
+  char word[80]; 
   
   cin >> num;
   for (int i = 1; i <= num; i++) {
     int erasenum = 0;
     word[0] = 0;
-    outword[0] = 0;
     
     cin >> erasenum;
     cin >> word;
     wordlen = strlen(word);
-    
-    if(erasenum > 1)
-      memmove( outword, word, erasenum-1);
-    memmove( outword + erasenum - 1, word + erasenum, wordlen - erasenum);
-    outword[wordlen-1] = 0;
 
-    cout << i << " " << outword << endl;
+    if(erasenum > 0) {
+      memmove( word + erasenum - 1, word + erasenum, wordlen - erasenum);
+      word[wordlen - 1] = 0;
+    }
+    
+    cout << i << " " << word << endl;
   }
   
   return 0;
