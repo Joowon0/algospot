a7f7e73f9024c8cb3d39331c35a8667d36c9e845
“Joowon” <joowon7508@gmail.com>
2016-07-09 16:44:12 +0900

    add comments

--- baseball.cpp
+++ baseball.cpp
@@ -1,4 +1,3 @@
-//#include <stdio.h>
 #include <iostream>
 #include <string>
 
@@ -13,29 +12,33 @@ int main () {
   float winRate[8];
   int biggerTeams;
 
+  //get case num
   cin >> cases;
 
   while(cases--) {
+    //get input
     for(i = 0; i< 8; i++) {
       cin >> names[i] >> scores[i][0] >> scores[i][1] >> scores[i][2];
     }
-    
+
+    //get myteam name
     cin >> playteam;
-    for(i = 0; i< 8; i++) {
+    for(i = 0; i< 8; i++) { // fint myteam place
       if (playteam == names[i]) {
 	myteam = i;
         break;
       }
     }
 
+    //get left play nums
     cin >> i;
-    i *= 2;
+    i *= 2; // 'case there's 2 for each play
     while(i--) {
       cin >> playteam;
-      if (playteam == names[myteam]) {
+      if (playteam == names[myteam]) { // if my team, wins
 	scores[myteam][0]++;
       }
-      else {
+      else { // if not myteam, lose (or draw)
 	for(int j = 0; j < 8; j++) {
 	  if (playteam == names[j]) {
 	    scores[j][2]++;
@@ -45,13 +48,13 @@ int main () {
       }
     }
 
+    // calcul winRate
     winRate[myteam] = (float) scores[myteam][0] / (scores[myteam][0]+scores[myteam][1]+scores[myteam][2]);
     biggerTeams = 0;
     for(i = 0; i < 8; i++) {
       winRate[i] =  (float) scores[i][0] / (scores[i][0]+scores[i][1]+scores[i][2]);
-
-      if (winRate[i] > winRate[myteam]) {
-	biggerTeams++;
+      if (winRate[i] > winRate[myteam]) { // if winRate is bigger than myteam
+	biggerTeams++; 
       }
       
     }
@@ -61,6 +64,7 @@ int main () {
     else
       cout << "NO" << endl;
     /*
+    // this is for testing
     for(i = 0; i< 8; i++) {
       cout << names[i] << " " << scores[i][0] << " "  <<
 	scores[i][1] << " " << scores[i][2] << " " << winRate[i] << endl;
